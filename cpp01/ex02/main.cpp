727ea2512f950543ba471692ba8fab1f323571d1
Soukaina Ouchen <souchen@e2r8p1.1337.ma>
2022-09-19 16:18:38 +0100

    ex02 finish

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <string.h>
+
 int main()
 {
-    int a;
-    a = 2;
-    int *p;
-    p=&a;
+    std::string variable = "HI THIS IS BRAIN";
+    std::string *stringPTR = &variable;
+    std::string &stringREF = variable;
 
-    std::cout << a;
-    std::cout<< "\n";
-    std::cout << &a;
-    std::cout<< "\n";
-    std::cout << p;
-    std::cout<< "\n";
-    std::cout << *p;
+    std::cout << "The memory address of the string variable => " << &variable << "\n";
+    std::cout << "The memory address held by stringPTR => " << stringPTR << "\n";
+    std::cout << "The memory address held by stringREF => " << &stringREF << "\n";
 
+    std::cout << "The value of the string variable => " << variable << "\n";
+    std::cout << "The value pointed to by stringPTR => " << *stringPTR << "\n";
+    std::cout << "The value pointed to by stringREF => " << stringREF << "\n";
 }
