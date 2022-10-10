d17811514f8cb8783f661e0ef2bd263ed1540468
Soukaina Ouchen <souchen@e2r8p1.1337.ma>
2022-10-10 20:03:46 +0100

    cpp01/ex00 update

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: souchen <souchen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 15:33:56 by souchen           #+#    #+#             */
-/*   Updated: 2022/09/23 11:38:52 by souchen          ###   ########.fr       */
+/*   Updated: 2022/10/10 20:03:11 by souchen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,10 +42,19 @@ void Fixed::setRawBits( int const raw )
 {
     this->a = raw;
 }
-
+/**the main difference between both is copy constructor creates a new object and assigns the value but assignment operator does not create a new object, instead it assigns the value to the data member of the same object.*/
 Fixed *Fixed::operator= (const Fixed &var)
 {
      this->a = var.a;
     std::cout << "Copy assignment operator called\n";
     return (this);
 }
+
+/**Copy Constructor initializes a new object by an already existing object of the same class.*/
+//The old object which was created and the new object which is invoked, will share different memory locations.
+/**Assignment operator is simply an operator which assigns some value to data members, objects.*/
+//The first object and second object which is assigned the value of first object, shares same memory locations.
+
+
+
+//The difference between a copy constructor and an assignment operator is that a copy constructor helps to create a copy of an already existing object without altering the original value of the created object, whereas an assignment operator helps to assign a new value to a data member or an object in the program.
